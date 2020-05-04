2823ea9649229d44d41b0a61616b94c753c326ab
perought <sduman1818@gmail.com>
2020-05-04 16:29:06 +0300

    fix indentation

--- add_post.php
+++ add_post.php
@@ -1,6 +1,6 @@
 <?php
-    include("templates/header.html");
-    include("templates/navbar.html");
+	include("templates/header.html");
+	include("templates/navbar.html");
 
 	include("config/db_connection.php");
 
@@ -8,20 +8,20 @@
 	$errors = array("name" => "", "email" => "", "title" => "", "post" => "");
 
 	if (isset($_POST["submit"])) {
-        if (empty($_POST["name"])) {
+		if (empty($_POST["name"])) {
 			$errors["name"] = "Name is required";
-        } 
-        else {
+		} 
+		else {
 			$name = $_POST["name"];
 			if (!preg_match("/^[a-zA-Z\s]+$/", $name)) {
 				$errors["name"] = "Name must be letters and spaces only";
 			}
-        }
-        
+		}
+		
 		if (empty($_POST["email"])) {
 			$errors["email"] = "An email is required";
-        } 
-        else {
+		} 
+		else {
 			$email = $_POST["email"];
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$errors["email"] = "Email must be a valid email address";
@@ -30,8 +30,8 @@
 
 		if (empty($_POST["title"])) {
 			$errors["title"] = "A title is required";
-        } 
-        else {
+		} 
+		else {
 			$title = $_POST["title"];
 			if (!preg_match("/^[a-zA-Z0-9\s+-]+$/", $title)) {
 				$errors["title"] = "Title must be letters, spaces and numbers only";
@@ -40,8 +40,8 @@
 
 		if (empty($_POST["post"])) {
 			$errors["post"] = "Post is required";
-        } 
-        else {
+		} 
+		else {
 			$post = $_POST["post"];
 			if (!preg_match("/^[a-zA-Z0-9\s\p{P}-]+$/", $post)) {
 				$errors["post"] = "post must be letters, spaces and numbers only";
@@ -49,72 +49,72 @@
 		}
 
 		if (!array_filter($errors)) {
-            $name = mysqli_real_escape_string($conn, $_POST["name"]);
-            $email = mysqli_real_escape_string($conn, $_POST["email"]);
-            $title = mysqli_real_escape_string($conn, $_POST["title"]);
-            $post = mysqli_real_escape_string($conn, $_POST["post"]);
-            
-            // echo $name . ", " . $email . ", " . $title . ", " . $post;
-    
-            $sql = "INSERT INTO blogs
-            (blog_id, blog_author, blog_author_email, blog_title, blog_body, blog_date) 
-            VALUES (
-            NULL, 
-            '$name', 
-            '$email', 
-            '$title', 
-            '$post', 
-            CURRENT_TIMESTAMP
-            );";
-    
-            if (mysqli_query($conn, $sql)) {
-                header("Location: index.php");
-            } 
-            else {
-                echo "query error: ". mysqli_error($conn);
-            }
-        } 
+			$name = mysqli_real_escape_string($conn, $_POST["name"]);
+			$email = mysqli_real_escape_string($conn, $_POST["email"]);
+			$title = mysqli_real_escape_string($conn, $_POST["title"]);
+			$post = mysqli_real_escape_string($conn, $_POST["post"]);
+			
+			// echo $name . ", " . $email . ", " . $title . ", " . $post;
+	
+			$sql = "INSERT INTO blogs
+			(blog_id, blog_author, blog_author_email, blog_title, blog_body, blog_date) 
+			VALUES (
+			NULL, 
+			'$name', 
+			'$email', 
+			'$title', 
+			'$post', 
+			CURRENT_TIMESTAMP
+			);";
+	
+			if (mysqli_query($conn, $sql)) {
+				header("Location: index.php");
+			} 
+			else {
+				echo "query error: ". mysqli_error($conn);
+			}
+		} 
 	} 
 ?>
 
 <div class="container">
-    <form class="form-horizontal" action="add_post.php" method="POST">
-        <div class="form-group">
-            <label class="control-label col-sm-2" for="text">Name:</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="name" name="name" value="<?php echo htmlspecialchars($name) ?>">
-                <span id="error"> <?php echo $errors["name"];?></span>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="control-label col-sm-2" for="email">Email:</label>
-            <div class="col-sm-10">
-                <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($email) ?>">
-                <span id="error"> <?php echo $errors["email"];?></span>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="control-label col-sm-2" for="text">Title:</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="title" name="title" value="<?php echo htmlspecialchars($title) ?>">
-                <span id="error"><?php echo $errors["title"];?></span>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="control-label col-sm-2" for="psta">Post:</label>
-            <div class="col-sm-10">
-                <textarea class="form-control" id="psta" rows="10" name="post"><?php echo htmlspecialchars($post) ?></textarea>
-                <span id="error"> <?php echo $errors["post"];?></span>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <input type="submit" name="submit" value="Submit">
-            </div>
-        </div>
-    </form>
+	<form class="form-horizontal" action="add_post.php" method="POST">
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="text">Name:</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="name" name="name" value="<?php echo htmlspecialchars($name) ?>">
+				<span id="error"> <?php echo $errors["name"];?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="email">Email:</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($email) ?>">
+				<span id="error"> <?php echo $errors["email"];?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="text">Title:</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="title" name="title" value="<?php echo htmlspecialchars($title) ?>">
+				<span id="error"><?php echo $errors["title"];?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="psta">Post:</label>
+			<div class="col-sm-10">
+				<textarea class="form-control" id="psta" rows="10" name="post"><?php echo htmlspecialchars($post) ?></textarea>
+				<span id="error"> <?php echo $errors["post"];?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<input type="submit" name="submit" value="Submit">
+			</div>
+		</div>
+	</form>
 </div>
 
 <?php 
-    include("templates/footer.html");
+	include("templates/footer.html");
 ?>
--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php
-    include("templates/header.html");
-    include("templates/navbar.html");
+	include("templates/header.html");
+	include("templates/navbar.html");
 
 	include("config/db_connection.php");
 
@@ -8,20 +8,20 @@
 		$id = mysqli_real_escape_string($conn, $_GET["id"]);
 
 		$sql = "SELECT * FROM blogs WHERE blog_id = $id";
-        $result = mysqli_query($conn, $sql);
+		$result = mysqli_query($conn, $sql);
 
 		$row = mysqli_fetch_assoc($result);
-    }
-    
-    if (isset($_POST["delete"])){
+	}
+	
+	if (isset($_POST["delete"])){
 		$id_to_delete = mysqli_real_escape_string($conn, $_POST["id_to_delete"]);
 
 		$sql = "DELETE FROM blogs WHERE blog_id = $id_to_delete";
 
 		if (mysqli_query($conn, $sql)){
 			header("Location: index.php");
-        } 
-        else {
+		} 
+		else {
 			echo "query error: " . mysqli_error($conn);
 		}
 
@@ -29,28 +29,28 @@
 ?>
 
 <div class="container mt-3"> 
-    <div class="row"> 
-        <div class="col-lg-8 col-md-10 mx-auto">
-            <?php if ($row) { ?>
-                <h2 class="post-title"> <?php echo $row["blog_title"] ?> </h2> 
-                <h4 class="post-subtitle"> <?php echo $row["blog_body"] ?> </h4> 
-                <p class="post-meta"> 
-                    Posted by <a href="#"> <?php echo $row["blog_author"] ?> </a> on <?php echo $row["blog_date"] ?> 
-                </p>
+	<div class="row"> 
+		<div class="col-lg-8 col-md-10 mx-auto">
+			<?php if ($row) { ?>
+				<h2 class="post-title"> <?php echo $row["blog_title"] ?> </h2> 
+				<h4 class="post-subtitle"> <?php echo $row["blog_body"] ?> </h4> 
+				<p class="post-meta"> 
+					Posted by <a href="#"> <?php echo $row["blog_author"] ?> </a> on <?php echo $row["blog_date"] ?> 
+				</p>
 
-                <form action="post.php" method="POST">
-				    <input type="hidden" name="id_to_delete" value="<?php echo $row['blog_id']; ?>">
-				    <input type="submit" name="delete" value="Delete">
-                </form>
-            
-            <?php } else { ?>
-                <h2 class="post-title"> Blog post not found. </h2> 
-            <?php } ?>
-            <hr>
-        </div> 
-    </div> 
+				<form action="post.php" method="POST">
+					<input type="hidden" name="id_to_delete" value="<?php echo $row['blog_id']; ?>">
+					<input type="submit" name="delete" value="Delete">
+				</form>
+			
+			<?php } else { ?>
+				<h2 class="post-title"> Blog post not found. </h2> 
+			<?php } ?>
+			<hr>
+		</div> 
+	</div> 
 </div>
 
 <?php 
-    include("templates/footer.html");
+	include("templates/footer.html");
 ?>
